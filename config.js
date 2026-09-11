36d62247893eff369489f6a7cc506f2a07d5c886
EDandG <ish.yasin@edandg.com>
2026-09-11 06:53:32 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
-  SUPABASE_URL: 'https://ffodzhudopcoppaqjtax.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_KMGkz09LI8SknjOm10IXdw_PomoxS2I',
+  SUPABASE_URL: 'https://qmipiulsdbrbtusxsesm.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_fC2_tG_TfNch9VUTkBdvAw_CkhZmcWO',
   USERNAME_DOMAIN: 'users.local',
   APP_NAME: 'Prescription Tracker by ED&G\u2122'
 }
